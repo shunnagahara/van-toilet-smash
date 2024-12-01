f9405dbb5bdef4f44c37a5455052a1ef4de8eb1f
shunnagahara <ringorian0626@gmail.com>
2024-11-30 18:05:34 -0800

    Github Actionsの設定を行った

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Static HTML出力を有効化
+  basePath: '/van-toilet-smash', // GitHubリポジトリ名を指定
+  images: {
+    unoptimized: true, // GitHub Pages用に画像最適化を無効化
+  },
+  assetPrefix: '/van-toilet-smash/', // アセットのプレフィックスを設定
 };
 
 export default nextConfig;
