65c3eb4d40924580ad750c9c4d535a55d5d68404
shunnagahara <ringorian0626@gmail.com>
2024-11-30 17:41:07 -0800

    backendソースの排除

--- backend/main/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .schema import Location, LocationResponse, BoundingBox, Image, LocalizedInfo
-from .database import DBLocation, DBImage, DBLocalizedInfo
-
-__all__ = [
-    'Location', 'LocationResponse', 'BoundingBox', 'Image', 'LocalizedInfo',
-    'DBLocation', 'DBImage', 'DBLocalizedInfo'
-]
