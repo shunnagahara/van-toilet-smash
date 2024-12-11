d22361495db9942f7f475f67b688ef527b442d40
shunnagahara <ringorian0626@gmail.com>
2024-12-10 23:49:05 -0800

    viewport-fit=coverを付けて、ステータスバー部分にWEB部分をレンダリングさせる

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+        <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover,user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
